95d7ed207512e7a4c892b80288bd5fcb4eb33df1
diepjess <jessicadiep97@gmail.com>
2025-02-28 18:29:21 -0500

    feat: Add basic implementation of HTMLNode class

---
+++ src/htmlnode.py
@@ -0,0 +1,24 @@
+class HTMLnode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+    
+    def to_html(self):
+        raise NotImplementedError
+    
+    def props_to_html(self):
+        if self.props is None:
+            return ""
+        return " " + " ".join(
+            f'{key}="{value}"'
+            for key, value in self.props.items()
+        )
+    
+    def __repr__(self):
+        tag = self.tag
+        value = self.value
+        children = self.children
+        props = self.props
+        return f"HTMLNode({tag},\n {value},\n {children},\n {props})"
