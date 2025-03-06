7e652b95c486326e27d40232fd723871749d802e
diepjess <jessicadiep97@gmail.com>
2025-03-06 17:38:15 -0500

    refactor: Renamed TestConverter to be more specific in testing text_node_to_html_node

--- src/test_converter.py
+++ src/test_converter.py
@@ -4,7 +4,7 @@ from converter import text_node_to_html_node
 from textnode import TextNode, TextType
 
 
-class TestConverter(unittest.TestCase):
+class TestTextNodeToHTMLNode(unittest.TestCase):
     def test_text(self):
         node = TextNode("This is a text node", TextType.TEXT)
         html_node = text_node_to_html_node(node)
